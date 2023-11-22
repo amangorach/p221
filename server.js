e6c3abc0c19d02446a3c75f03cdf91a0db784401
amangorach <91610050+amangorach@users.noreply.github.com>
2023-11-22 19:40:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.use(express.json())
+
+var nodemailer = require('nodemailer');
+
+server.listen(process.env.PORT || 3030);
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+    user: 'rahilagorach@gmail.com',
+    pass: '',
+    },
+    secure: true,
+    });
